25e9dc7de2f1b3e2bc79ee53ff75e44678ef055a
Alexander Winkowski <dereference23@outlook.com>
2021-09-30 20:54:59 +0530

    m51: Import cleaned up powerhint
    Signed-off-by: Arjun-Ingole <arjuningole38@gmail.com>
    Change-Id: I3d16e41487873dd25e51f0c8c9e2ecc0642b3c42

--- device.mk
+++ device.mk
@@ -411,7 +411,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.3.vendor \
 
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/rootdir/vendor/etc/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
+    $(DEVICE_PATH)/rootdir/vendor/etc/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
 
 # Protobuf
 PRODUCT_PACKAGES += \
