5adc2233c4a9e181a62ca24c16af9f1932234e95
LuK1337 <priv.luk@gmail.com>
2022-03-16 01:30:14 +0100

    m51: Remove trust HAL from product packages
    USB 1.3 HAL now provides the same functionality and even more.
    
    Change-Id: Idda908a90d3b935e1065eb41abec4babeefc4fd0

--- device.mk
+++ device.mk
@@ -608,10 +608,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.samsung \
 
-# Trust
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service \
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti \
