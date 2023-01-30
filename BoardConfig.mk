6ea0550a4c222f409f3281bba87ff9a19a9c587f
Edwin Moquete <edwinmmoquete@gmail.com>
2023-01-30 22:04:26 +0100

    m51: Switch to legacy sepolicy_vndr
    Change-Id: I6cdd2b6811780e8efc7bca72f0ed480256734d32

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -263,7 +263,7 @@ VENDOR_SECURITY_PATCH := 2023-07-01
 
 # SEPolicy
 TARGET_SEPOLICY_DIR := msmsteppe
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
