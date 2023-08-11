0dca185a66e449e475888539e4f940118eaf44ba
Fenglin Wu <fenglinw@codeaurora.org>
2023-08-11 18:03:25 +0200

    m51: Add initial version of QTI vibrator HAL implementation
    QTI vibrator HAL implementation controls vibrator device registered
    as input force-feedback device through ioctl/write() calls. It supports
    turning on/off vibrator, setAmplitude, and perform effects.
    
    Change-Id: Ic099cbefdee1f10c83d92a19be0f79c21867ee6f

--- device.mk
+++ device.mk
@@ -662,6 +662,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndservicemanager \
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.impl \
+    vendor.qti.hardware.vibrator.service \
+
 # WiFi
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay \
