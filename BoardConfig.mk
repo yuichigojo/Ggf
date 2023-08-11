3495d93f4ac08244d62def961f8e70db351fad13
Jiyong Park <jiyong@google.com>
2023-08-11 02:23:08 +0200

    m51: Declare support for updatable APEXes
    Change-Id: Ic03589cc3a32c3fa64b939a935f664a25a4d6f4e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,9 @@ DEVICE_PATH := device/samsung/m51
 # 64 Bit
 TARGET_USES_64_BIT_BINDER := true
 
+# APEX
+DEXPREOPT_GENERATE_APEX_IMAGE := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
